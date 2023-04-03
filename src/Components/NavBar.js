d4cfdfad4dc72b9b74daad8819ba38845b02b9ed
peanutyabing <sherry.liuyabing@gmail.com>
2023-04-03 21:43:34 +0800

    Hide filter tab in navbar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -6,7 +6,7 @@ export default function NavBar() {
       <Container>
         <Navbar.Brand href="/">Bigfoot sightings</Navbar.Brand>
         <Nav className="me-auto">
-          <Nav.Link href="/filter">Filter</Nav.Link>
+          {/* <Nav.Link href="/filter">Filter</Nav.Link> */}
           <Nav.Link href="/report-sighting">Report a sighting</Nav.Link>
         </Nav>
       </Container>
