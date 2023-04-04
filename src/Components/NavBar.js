5b79ffae23615ba8256d2b36c690742a58dbd235
peanutyabing <sherry.liuyabing@gmail.com>
2023-04-04 09:55:21 +0800

    Fix reload on navigate issue by changing href to useNavigate

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,13 +1,27 @@
 import { Container, Nav, Navbar } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 export default function NavBar() {
+  const navigate = useNavigate();
   return (
     <Navbar bg="light" variant="light">
       <Container>
         <Navbar.Brand href="/">Bigfoot sightings</Navbar.Brand>
         <Nav className="me-auto">
-          {/* <Nav.Link href="/filter">Filter</Nav.Link> */}
-          <Nav.Link href="/report-sighting">Report a sighting</Nav.Link>
+          {/* <Nav.Link
+            onClick={() => {
+              navigate("/filter");
+            }}
+          >
+            Filter
+          </Nav.Link> */}
+          <Nav.Link
+            onClick={() => {
+              navigate("/report-sighting");
+            }}
+          >
+            Report a sighting
+          </Nav.Link>
         </Nav>
       </Container>
     </Navbar>
